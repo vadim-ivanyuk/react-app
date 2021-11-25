2a9d6b0e7418db238da95ec8dcf1f7ad654c9779
Vadym Ivaniuk <vadym_ivaniuk@epam.com>
2021-11-25 11:37:49 +0200

    fixed axios

--- src/helpers/myCustomAxios.js
+++ src/helpers/myCustomAxios.js
@@ -4,35 +4,19 @@ import { API_URL } from '../utils/apies';
 
 const session_key = localStorage.getItem('session_key');
 
+axios.defaults.headers.common['Authorization'] = session_key;
+
 export const myCustomAxios = {
 	get: (path, data) => {
-		return axios.get(`${API_URL}${path}`, {
-			headers: {
-				Authorization: session_key,
-			},
-			data,
-		});
+		return axios.get(`${API_URL}${path}`, data);
 	},
 	post: (path, data) => {
-		return axios.post(`${API_URL}${path}`, data, {
-			headers: {
-				Authorization: session_key,
-			},
-		});
+		return axios.post(`${API_URL}${path}`, data);
 	},
 	put: (path, data) => {
-		return axios.put(`${API_URL}${path}`, data, {
-			headers: {
-				Authorization: session_key,
-			},
-		});
+		return axios.put(`${API_URL}${path}`, data);
 	},
 	delete: (path, data) => {
-		return axios.delete(`${API_URL}${path}`, {
-			headers: {
-				Authorization: session_key,
-			},
-			data,
-		});
+		return axios.delete(`${API_URL}${path}`, data);
 	},
 };
